ff31dd38676c722afabe2ebbb074a4596eb5dff2
Richard Iwasa <riwasa@microsoft.com>
2021-11-22 11:04:10 -0500

    Renamed Resource Group script files. Added Log Analytics Workspace files.

---
+++ management-and-governance/monitor/log-analytics-workspace-arm.ps1
@@ -0,0 +1,31 @@
+# *****************************************************************************
+#
+# File:        log-analytics-workspace.ps1
+#
+# Description: Creates a Log Analytics Workspace.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+# 
+# *****************************************************************************
+
+# Get script variables.
+$resourceGroupName = "<resource-group-name>"
+$location = "<resource-group-location>"
+$workspaceName = "<log-analytics-workspace-name>"
+
+# Create a Log Analytics Workspace.
+Write-Host "Creating a Log Analytics Workspace"
+
+New-AzResourceGroupDeployment `
+	-Name "log-analytics-workspace" `
+	-ResourceGroupName "$resourceGroupName" `
+	-TemplateFile "$PSScriptRoot\log-analytics-workspace.azuredeploy.json" `
+  -TemplateParameterFile "$PSScriptRoot\log-analytics-workspace.azuredeploy.parameters.json" `
+	-workspaceName "$workspaceName" `
+	-location "$location"
