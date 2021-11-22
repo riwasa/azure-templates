476a2c42f52a5ebdea5a7888ef9f0c1d3ec66911
Richard Iwasa <riwasa@microsoft.com>
2021-11-22 15:55:37 -0500

    Added new Storage Account files

---
+++ storage/storage_account-blob_files-arm.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# *****************************************************************************
+#
+# File:        storage_account-blob_files-arm.sh
+#
+# Description: Creates a Storage Account, configuring blob and basic file
+#              services.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+#
+# *****************************************************************************
+
+# Get script variables.
+resourceGroupName="<resource-group-name>"
+location="<resource-group-location>"
+storageAccountName="<storage-account-name>"
+
+# Create a Storage Account.
+echo "Creating a Storage Account"
+
+az deployment group create \
+  --name "storage-account-blob-files" \
+  --resource-group "$resourceGroupName" \
+  --template-file "storage-account-blob-files.azuredeploy.json" \
+  --parameters @"storage-account-blob-files.azuredeploy.parameters.json" \
+  --parameters storageAccountName="$storageAccountName" \
+               location="$location"  
